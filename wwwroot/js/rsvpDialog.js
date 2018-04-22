2d2f83d462b9b4679db6ad92223de20fbdabc4bd
Valeh Nasser <valeh@valeh.ca>
2018-04-22 12:31:27 -0600

    dialog inputs enable and disable

--- wwwroot/js/rsvpDialog.js
+++ wwwroot/js/rsvpDialog.js
@@ -4,28 +4,43 @@ var rsvpDialog = function() {
 	var initFromModel= function (model) {
 		if (model.rsvpStatus != "Unknown" )
 		{	
-			switch (model.rsvpStatus)
-			{
-				case "Yes": 
-					$("#rsvpValueYes").attr("checked","");
-					break;
-				case "YesPlusOne": 
-					$("rsvpValueYesPlusOne").attr("checked","");
-					break;
-				case "No": 
-					$("rsvpValueNo").attr("checked","");
-					break;
-			}
+			$("#rsvpValue"+model.rsvpStatus).prop("checked",true);
 		}
 		$("#numberOfKids").val(model.numberOfKids);
-		$("#plusOneName").val(model.plusOneName);		 
+		$("#plusOneName").val(model.plusOneName);	
+		resetEnabledInputs();	 
 	};	
 
 
 	var closeDialog= function(){
 		$('#rsvpUdateDialog').modal("hide");
 	};
-	
+
+	var resetEnabledInputs = function()
+	{
+		var value = $("[name=rsvp-value]:checked").val();
+		if (value == "No") {
+			$("#plusOneName" ).prop( "disabled", true );
+			$("#plusOneName").val("");
+
+			$("#numberOfKids").prop( "disabled", true );
+			$("#numberOfKids").val("0");
+		}
+		else if (value == "Yes") {
+			$("#numberOfKids").prop( "disabled", false );
+
+			$("#plusOneName" ).prop( "disabled", true );
+			$("#plusOneName").val("");
+		}
+		else { //yes plus one
+			$("#plusOneName" ).prop( "disabled", false );
+			$("#numberOfKids").prop( "disabled", false );
+		}
+	};
+		
+	$("[name=rsvp-value]").change(resetEnabledInputs);
+
+
 	return {
 		closeDialog: closeDialog,
 		initFromModel: initFromModel
